c2e604a94fcfc688f13bf75a1a035faa5e328af4
Valeria Ca <12345678+czvaleria@users.noreply.github.com>
2026-03-06 23:17:34 -0600

    Listo javadoc

--- src/Producto.java
+++ src/Producto.java
@@ -1,5 +1,29 @@
+/**
+ * Interfaz que representa un producto que puede ser ordenado por el cliente.
+ *
+ * Define los métodos básicos que todo producto debe implementar,
+ * como su nombre, descripción y precio.
+ */
 public interface Producto{
+
+    /**
+     * Regresa el nombre del producto.
+     *
+     * @return el nombre del producto.
+     */
     String getNombre();
+
+    /**
+     * Regresa la descripción del producto.
+     *
+     * @return la descripción del producto.
+     */
     String getDescripcion();
+
+    /**
+     * Regresa el precio del producto.
+     *
+     * @return el precio del producto.
+     */
     double getPrecio();
 }
